a3a93451c3f8a90e8b5947696ba6cfd373c0365e
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-05 02:23:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,9 @@ def key_for_min_value(name_hash)
 smallest_key=nil
 smallest_value = 10000
 name_hash.each do |key, number| 
-  if number>smallest_value
+  if number<=smallest_value
     smallest_value=number
-  else
-    smallest_value=smallest_value
   end
-end
 name_hash.key(smallest_value)
+end
 end
